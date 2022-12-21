ee4a56c54e612a99b1ce6d2cbc92ddb00676c252
ycd633 <ycd633@naver.com>
2022-12-21 15:39:31 +0900

    init

---
+++ src/test/java/com/git/sample1/security/SecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.git.sample1.security;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
